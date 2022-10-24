38566eb45578fc3fd79921a12bda0d7ba3270c61
piokaf05@gmail.com <piokaf05@gmail.com>
2022-10-24 18:20:48 +0200

    Main menu page 'index.php'

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>League Online</title>
+    <link rel="stylesheet" href="styles/main-page.css">
+</head>
+<body>
+    <div id="logo">
+        <img src="images/cup.png" alt="cup">
+        <div id="logo-name">League <span>Online</span></div>
+        <div style="clear: both;"></div>
+    </div>
+    <div id="container"></div>
+    <script src="scripts/js-library.js"></script>
+    <script src="scripts/page-script.js"></script>
+</body>
+</html>
